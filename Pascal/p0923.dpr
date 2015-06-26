26b439316d9845faf086483061d6e75b54ca095c
maxkrivich <maxkrivich@gmail.com>
2015-06-26 16:49:04 +0300

    Pascal

---
+++ Pascal/p0923.dpr
@@ -0,0 +1,14 @@
+program p923;
+{$APPTYPE CONSOLE}
+uses SysUtils;
+var
+  n: 1..12;
+begin
+  read(n);
+  case n of
+    12, 1, 2: Writeln('Winter');
+    9, 10, 11: Writeln('Autumn');
+    3, 4, 5: Writeln('Spring');
+    6, 7, 8: Writeln('Summer');
+  end;
+end.
