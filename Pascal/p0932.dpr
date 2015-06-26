3c8711eaf48bd0d260a5fe37b1d3fad60c8a0346
maxkrivich <maxkrivich@gmail.com>
2015-06-26 23:21:33 +0300

    Pascal

---
+++ Pascal/p0932.dpr
@@ -0,0 +1,13 @@
+program p932;
+{$APPTYPE CONSOLE}
+uses
+  SysUtils;
+var
+  s, a, h, d: real;
+begin
+  read(s);
+  read(a);
+  d := a * a + 8 * s;
+  h := (-a + sqrt(d)) / 2;
+  writeln(h: 0: 2);
+end.
