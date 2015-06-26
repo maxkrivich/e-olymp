bfa3ac2bc2fac1d1d46261432590ec38477524c0
maxkrivich <maxkrivich@gmail.com>
2015-06-26 23:31:28 +0300

    Pascal

---
+++ Pascal/p4723.dpr
@@ -0,0 +1,14 @@
+program p4723;
+{$APPTYPE CONSOLE}
+uses SysUtils;
+
+var
+  i, n: integer;
+  sum: int64;
+begin
+  sum := 0;
+  readln(n);
+  for i := 1 to n do
+    sum := sum + i * i;
+  writeln(sum);
+end.
