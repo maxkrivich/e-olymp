3dde7ae8601194957ac5dd83a5f4ace78f6f18ed
maxkrivich <maxkrivich@gmail.com>
2015-06-26 16:45:21 +0300

    Java

---
+++ Java/p0273.java
@@ -0,0 +1,14 @@
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        BigInteger a = new BigInteger(in.next().trim()),
+                b = new BigInteger(in.next().trim()),
+                m = new BigInteger(in.next().trim());
+        System.out.println(a.modPow(b, m));
+    }
+
+}
