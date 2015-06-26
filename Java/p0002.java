4fb9f4f9f3b4846f13b557a97854fe36453e9d44
maxkrivich <maxkrivich@gmail.com>
2015-06-26 23:13:44 +0300

    Java

---
+++ Java/p0002.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Main {
+
+  public static void main(String[] args) throws Exception{
+    System.out.println(new Scanner(System.in).next().length());
+  }
+
+}
