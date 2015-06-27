5da44dfd01f45e0f1f2852b7351c152529f71548
maxkrivich <maxkrivich@gmail.com>
2015-06-27 21:16:20 +0300

    Pascal

---
+++ Pascal/p0020.dpr
@@ -0,0 +1,25 @@
+program p20;
+{$APPTYPE CONSOLE}
+uses
+  SysUtils;
+
+var
+  n, cnt, m: longint;
+  sum: integer;
+begin
+  readln(n);
+  cnt := 0;
+  while n > 0 do
+  begin
+    sum := 0;
+    m := n;
+    while m > 0 do
+    begin
+      sum := sum + m mod 10;
+      m := m div 10;
+    end;
+    n := n - sum;
+    inc(cnt);
+  end;
+  writeln(cnt);
+end.
