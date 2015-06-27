360174756768d831bc226ec4c9307e8378565f07
maxkrivich <maxkrivich@gmail.com>
2015-06-27 19:11:01 +0300

    Java

--- Java/p0930.java
+++ Java/p0930.java
@@ -6,21 +6,22 @@ import java.io.InputStreamReader;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        String number = new BufferedReader(new InputStreamReader(System.in)).readLine(), str = "";
+        String number = new BufferedReader(new InputStreamReader(System.in)).readLine();
+        StringBuilder str = new StringBuilder();
         byte cnt = 0;
         for (char i = '0'; i < ':'; i++) {
-            if (!number.contains(""+i)) {
+            if (!number.contains("" + i)) {
                 ++cnt;
-                str += i + " ";
+                str.append(i + " ");
             }
         }
-      if(cnt==0){
-          System.out.println("0");
-      }else{
-          System.out.println(cnt);
-          System.out.println(str.trim());
-      }
-          
+        if (cnt == 0) 
+            System.out.println("0");
+         else {
+            System.out.println(cnt);
+            System.out.println(str.toString().trim());
+        }
+
     }
 
 }
