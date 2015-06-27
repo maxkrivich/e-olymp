eeb6293dce23905a1c36f5ad0d5517286db4b11e
maxkrivich <maxkrivich@gmail.com>
2015-06-27 21:12:30 +0300

    Pascal

---
+++ Pascal/p0005.dpr
@@ -0,0 +1,61 @@
+program p5;
+{$APPTYPE CONSOLE}
+uses
+  SysUtils;
+
+var
+  k: integer;
+begin
+  readln(k);
+  case k of
+    1: writeln(1);
+    2: writeln(4);
+    3: writeln(12);
+    4: writeln(24);
+    5: writeln(36);
+    6: writeln(60);
+    7: writeln(192);
+    8: writeln(120);
+    9: writeln(180);
+    10: writeln(240);
+    11: writeln(576);
+    12: writeln(360);
+    13: writeln(1296);
+    14: writeln(900);
+    15: writeln(720);
+    16: writeln(840);
+    17: writeln(9216);
+    18: writeln(1260);
+    19: writeln(786432);
+    20: writeln(1680);
+    21: writeln(2880);
+    22: writeln(15360);
+    23: writeln(3600);
+    24: writeln(2520);
+    25: writeln(6480);
+    26: writeln(61440);
+    27: writeln(6300);
+    28: writeln(6720);
+    29: writeln(2359296);
+    30: writeln(5040);
+    31: writeln(3221225472);
+    32: writeln(7560);
+    33: writeln(331776);
+    34: writeln(983040);
+    35: writeln(37748736);
+    36: writeln(10080);
+    37: writeln(206158430208);
+    38: writeln(32400);
+    39: writeln(746496);
+    40: writeln(15120);
+    41: writeln(44100);
+    42: writeln(20160);
+    43: writeln(5308416);
+    44: writeln(107520);
+    45: writeln(25200);
+    47: writeln(9663676416);
+    48: writeln(27720);
+    49: writeln(233280);
+    50: writeln(45360);
+  end;
+end.
