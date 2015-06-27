2273e1d212758890817c85d9ef9058e3e4c4acec
maxkrivich <maxkrivich@gmail.com>
2015-06-27 17:52:04 +0300

    C++

---
+++ С++/p0063.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using std::cin;
+using std::cout;
+
+int main(){
+	__int64* n = new __int64;
+	__int64* m = new __int64;
+	cin >> *n >> *m;
+	cout << (*n - 1) * (*m - 1) + 1 << "\n";
+	delete n, m;
+	return 0;
+}
