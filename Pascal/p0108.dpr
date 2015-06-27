83ca34d5d9adb119f9767ff5b2a99287f217e59f
maxkrivich <maxkrivich@gmail.com>
2015-06-27 21:18:56 +0300

    Pascal

---
+++ Pascal/p0108.dpr
@@ -0,0 +1,20 @@
+program p108;
+{$APPTYPE CONSOLE}
+uses
+  SysUtils;
+
+var
+  M: array[1..3] of Integer;
+  i, j, tmp: Integer;
+begin
+  Read(M[1], M[2], M[3]);
+  for i := 3 - 1 downto 1 do
+    for j := 1 to i do
+      if M[j] > M[j + 1] then
+      begin
+        tmp := M[j];
+        M[j] := M[j + 1];
+        M[j + 1] := tmp;
+      end;
+  Writeln(M[2]);
+end.
