b8c9c9bcfbb404c267084fa9ad486f600284d67b
maxkrivich <maxkrivich@gmail.com>
2015-07-04 20:14:40 +0300

    Установить бит

---
+++ С++/p5315.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using std::cin;
+using std::cout;
+
+
+int main(){
+	int* a = new int;
+	int* k = new int;
+	cin >> *a >> *k;
+	cout << (int)(*a | (1 << *k)) << "\n";
+	delete a, k;
+	return 0;
+}
