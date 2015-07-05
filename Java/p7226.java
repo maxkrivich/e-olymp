9755606a3bc84452eb69a44da343c13858163b28
maxkrivich <maxkrivich@gmail.com>
2015-07-05 17:38:06 +0300

    День календаря

--- Java/p7226.java
+++ Java/p7226.java
@@ -1,3 +1,4 @@
+
 import java.util.GregorianCalendar;
 import java.util.Calendar;
 import java.util.Scanner;
@@ -5,11 +6,10 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        int day = in.nextInt();
+        int day = new Scanner(System.in).nextInt();
         GregorianCalendar g = new GregorianCalendar(2014, Calendar.ALL_STYLES, day);
-        System.out.println(g.get(Calendar.DAY_OF_MONTH)+" "+(g.get(Calendar.MONTH)+1));
-
+        System.out.println(g.get(Calendar.DAY_OF_MONTH) + " " + (g.get(Calendar.MONTH) + 1));
+        
     }
 
 }
