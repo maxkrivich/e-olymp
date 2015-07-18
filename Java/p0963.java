144471d44ece6762370041fd28b6a570bb0b9a71
maxkrivich <maxkrivich@gmail.com>
2015-07-18 18:16:04 +0300

    Перестановка слов

---
+++ Java/p0963.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+        java.util.Scanner in = new java.util.Scanner(System.in);
+        String ln = in.next(), fn = in.next();
+        System.out.println(fn + " " + ln);
+    }
+
+}
