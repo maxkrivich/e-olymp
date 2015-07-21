e0ca91c4feb1dd55a8f79a175651d431625952f6
maxkrivich <maxkrivich@gmail.com>
2015-07-22 00:24:52 +0300

    Calendar

---
+++ Pascal/p1955.dpr
@@ -0,0 +1,15 @@
+program Project1;
+{$APPTYPE CONSOLE}
+
+uses SysUtils;
+
+var
+  d1, d2, m1, m2, y1, y2: Integer;
+
+begin
+  read(d1, m1, y1);
+  read(d2, m2, y2);
+  if ((m1 = m2) and (d2 < d1)) or (m2 < m1) then
+    Inc(y1);
+  Writeln(y2 - y1);
+end.
