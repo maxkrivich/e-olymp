74bb1ab419bf306782a61a372937785ba068db73
maxkrivich <maxkrivich@gmail.com>
2015-07-25 20:23:23 +0300

    Number Of Days In Month

--- С++/p6279.cpp
+++ С++/p6279.cpp
@@ -3,7 +3,6 @@
 int getNumberOfDays(int m, int y){
 	int leap = (1 - (y % 4 + 2) % (y % 4 + 1)) * ((y % 100 + 2) % (y % 100 + 1)) + (1 - (y % 400 + 2) % (y % 400 + 1));
 	return 28 + ((m + (m / 8)) % 2) + 2 % m + ((1 + leap) / m) + (1 / m) - (leap / m);
-
 }
 
 int main(){
