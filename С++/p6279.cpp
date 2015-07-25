d8df9fc097ecaa36a4513fd7c3de67794cab2e30
maxkrivich <maxkrivich@gmail.com>
2015-07-25 20:22:38 +0300

    Number Of Days In Month

---
+++ С++/p6279.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int getNumberOfDays(int m, int y){
+	int leap = (1 - (y % 4 + 2) % (y % 4 + 1)) * ((y % 100 + 2) % (y % 100 + 1)) + (1 - (y % 400 + 2) % (y % 400 + 1));
+	return 28 + ((m + (m / 8)) % 2) + 2 % m + ((1 + leap) / m) + (1 / m) - (leap / m);
+
+}
+
+int main(){
+	int n, m;
+	scanf("%d %d", &n, &m);
+	printf("%d\n",getNumberOfDays(n,m));
+	return 0;
+}
