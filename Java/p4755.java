ac31ae97c3f4a7a9d7455386681cfd981baec92d
maxkrivich <maxkrivich@gmail.com>
2015-08-11 13:25:07 +0300

    Из десятичной в тринадцатиричную

---
+++ Java/p4755.java
@@ -0,0 +1,7 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(new java.math.BigInteger(new java.util.Scanner(System.in).nextLine()).toString(13).toUpperCase());
+    }
+
+}
