29fa4842603420fb9100ae56b3e2a4dc642dab52
Max Krivich <maxkrivich@users.noreply.github.com>
2015-11-28 23:56:05 +0300

    Сумма чисел на промежутке

---
+++ С++/p2860.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+void sum(long &pt) {
+	pt = pt*(pt + 1) / 2;
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(false);	
+	long a, b;
+	cin >> a >> b;
+	if (a > b) swap(a,b);
+	sum(b);
+	sum(--a);
+	cout << b - a << endl;
+	return 0;
+}
